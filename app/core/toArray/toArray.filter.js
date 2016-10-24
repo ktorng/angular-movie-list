304a570cd4364d414b1ab1183adfe4d9198494b9
Kenneth Torng <ktorng@gmail.com>
2016-10-24 18:44:16 -0500

    (fix) Sort by rating correctly by using -1 to represent N/A

--- app/core/toArray/toArray.filter.js
+++ app/core/toArray/toArray.filter.js
@@ -1,11 +1,12 @@
-// filter to transform object to array for repeater
+// filter to transform movie object to array for repeater
 angular
   .module('core')
   .filter('toArray', function() {
     return function(obj) {
       const result = [];
-      angular.forEach(obj, (value) => {
-        result.push(value);
+      angular.forEach(obj, (movie) => {
+        movie.myRating = movie.myRating || -1;
+        result.push(movie);
       });
       return result;
     }
